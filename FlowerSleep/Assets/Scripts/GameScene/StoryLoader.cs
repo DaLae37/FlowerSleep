32c39650ccc3fae1c865f5aed02a6dc28171996e
DaLae37 <dalae37@gmail.com>
2018-04-12 10:30:44 +0900

    문자열 파싱 성공

---
+++ FlowerSleep/Assets/Scripts/GameScene/StoryLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StoryLoader : MonoBehaviour {
+    public string storyPath = "Assets/Resources/Stories/"; //Set Story TextFile's Location
+    int storyPhase; //Choosing What Story
+    public static ArrayList story = new ArrayList(); //
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.HasKey("storyPhase"))
+        {
+            storyPhase = PlayerPrefs.GetInt("storyPhase");
+        }
+        else
+        {
+            storyPhase = 0;
+        }
+        FileStream fs = new FileStream(storyPath + storyPhase + ".txt", FileMode.Open);
+        StreamReader sr = new StreamReader(fs);
+        string readStr;
+        while((readStr = sr.ReadLine())!=null){
+            story.Add(readStr);
+            Debug.Log(readStr);
+        }
+        sr.Close();
+        fs.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FlowerSleep/Assets/Scripts/MainScene/MainSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainSceneManager : MonoBehaviour {
+    public void GameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("LoadScene");
+    }
+    public void SettingScene()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+    public void GalleryScene()
+    {
+        SceneManager.LoadScene("GalleryScene");
+    }
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
