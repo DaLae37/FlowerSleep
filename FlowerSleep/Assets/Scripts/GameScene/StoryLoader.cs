b5529a444e1b4bbc59c14fc6f43f80a314bfe921
DaLae37 <dalae37@gmail.com>
2018-04-23 22:38:27 +0900

    뮤트 설정 유지 및 스토리 한 글자씩 출력

--- FlowerSleep/Assets/Scripts/GameScene/StoryLoader.cs
+++ FlowerSleep/Assets/Scripts/GameScene/StoryLoader.cs
@@ -15,31 +15,50 @@ public class StoryLoader : MonoBehaviour {
     public Text StoryText;
     public Text NameText;
 
-	// Use this for initialization
-	void Start () {
+    private bool isChainingDone;
+    private string chainingString;
+    private int chainingStringIndex;
+    // Use this for initialization
+    void Start () {
         if (PlayerPrefs.HasKey("storyPhase")) //Initialization values
             storyPhase = PlayerPrefs.GetInt("storyPhase");
         else
             storyPhase = 0;
         storyIndex = 0;
+        isChainingDone = false;
+        chainingStringIndex = 0;
 
         SetStory();
         SetText();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.anyKeyDown)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
         {
-            SetText();
+            if (isChainingDone)
+            {
+                isChainingDone = false;
+                chainingStringIndex = 0;
+                SetText();
+            }
+            else
+            {
+                StopCoroutine("ChainingText");
+                isChainingDone = true;
+                StoryText.text = chainingString;
+            }
         }
-	}
+    }
+
     public void Vibrate()
     {
         #if UNITY_ANDROID
         Handheld.Vibrate();
         #endif
     }
+
     public void SetStory() //Reading StoryText at TextFile's Location
     {
         FileStream fs = new FileStream(storyPath + storyPhase + ".txt", FileMode.Open);
@@ -52,6 +71,7 @@ public class StoryLoader : MonoBehaviour {
         sr.Close();
         fs.Close();
     }
+
     public void SetText()
     {
         string tmp = story[storyIndex++].ToString();
@@ -59,12 +79,23 @@ public class StoryLoader : MonoBehaviour {
         {
             string[] tmps = tmp.Split(':');
             NameText.text = tmps[0].Trim();
-            StoryText.text = tmps[1].Trim();
+            chainingString = tmps[1].Trim();
         }
         else
         {
             NameText.text = "";
-            StoryText.text = tmp;
+            chainingString = tmp;
         }
+        StartCoroutine("ChainingText");
+    }
+
+    IEnumerator ChainingText()
+    {
+        do
+        {
+            StoryText.text = chainingString.Substring(0, chainingStringIndex++);
+            yield return new WaitForSeconds(0.1f);
+        } while (chainingStringIndex != chainingString.Length);
+        isChainingDone = true;
     }
 }
--- FlowerSleep/Assets/Scripts/SettingScene/SettingSceneManager.cs
+++ FlowerSleep/Assets/Scripts/SettingScene/SettingSceneManager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class SettingSceneManager : MonoBehaviour {
-    public Slider BGMBar;
-    public Slider SEBar;
-    public Toggle BGMMute;
-    public Toggle SEMute;
-    
+    public Slider BGM_Slider;
+    public Slider SE_Slider;
+
+    public Toggle BGM_Mute;
+    public Toggle SE_Mute;
+
+    private bool isBGM_Mute;
+    private bool isSE_Mute;
+
 	// Use this for initialization
 	void Start () {
-		if(PlayerPrefs.HasKey("bgm") && PlayerPrefs.HasKey("se")){
-				BGMBar.value = PlayerPrefs.GetFloat("bgm");
-				SEBar.value = PlayerPrefs.GetFloat("se");
-		}
-		else{
-				BGMBar.value = 0;
-				SEBar.value = 0;
-		}
+        if (PlayerPrefs.HasKey("bgm") && PlayerPrefs.HasKey("se"))
+        {
+            BGM_Slider.value = PlayerPrefs.GetFloat("bgm"); //Getting before BGM Slider's value(0~1)
+            SE_Slider.value = PlayerPrefs.GetFloat("se"); //Same to above
+
+            isBGM_Mute = (PlayerPrefs.GetInt("bgm_mute") == 1) ? true : false; //Setting on mute botton If before setting on
+            isSE_Mute = (PlayerPrefs.GetInt("se_mute") == 1) ? true : false; //Same to above
+        }
+        else //Resetting if don't have before setting
+        {
+            BGM_Slider.value = 1;
+            SE_Slider.value = 1;
+
+            isBGM_Mute = false;
+            isSE_Mute = false;
+        }
+        BGM_Mute.isOn = isBGM_Mute;
+        SE_Mute.isOn = isSE_Mute;
 	}
 	
 	// Update is called once per frame
@@ -28,8 +42,34 @@ public class SettingSceneManager : MonoBehaviour {
 
     public void MainScene()
     {
-		PlayerPrefs.SetFloat("bgm",BGMBar.value);
-		PlayerPrefs.SetFloat("se",SEBar.value);
+		PlayerPrefs.SetFloat("bgm",BGM_Slider.value); //Save BGM Slider's value
+		PlayerPrefs.SetFloat("se",SE_Slider.value); //Save SE Slider's value
+
+        PlayerPrefs.SetInt("bgm_mute", (isBGM_Mute) ? 1 : 0); //Save BGM is mute
+        PlayerPrefs.SetInt("se_mute", (isSE_Mute) ? 1 : 0); //Save SE is mute
+
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void BGMSliderChanged() //Invoke this function When BGM Slider is changed
+    {
+        if (isBGM_Mute)
+            isBGM_Mute = false;
+    }
+
+    public void SESliderChanged() //Invoke this function When SE Slider is changed
+    {
+        if (isSE_Mute)
+            isSE_Mute = false;
+    }
+
+    public void BGMMuteChanged() //Invoke this function When BGM Mute is clicked
+    {
+        isBGM_Mute = !isBGM_Mute;
+    }
+
+    public void SEMuteChanged() //Invoke this function When SE Mute is clicked
+    {
+        isSE_Mute = !isSE_Mute;
     }
 }
