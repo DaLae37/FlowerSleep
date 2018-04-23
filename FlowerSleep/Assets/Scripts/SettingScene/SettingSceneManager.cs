34b11c546fb55961738b7dd04828b56a716bc5cf
DaLae37 <dalae37@gmail.com>
2018-04-23 13:24:16 +0900

    사운드 설정 유지

--- FlowerSleep/Assets/Scripts/SettingScene/SettingSceneManager.cs
+++ FlowerSleep/Assets/Scripts/SettingScene/SettingSceneManager.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class SettingSceneManager : MonoBehaviour {
-    public Image BGMBar;
-    public Image SEBar;
+    public Slider BGMBar;
+    public Slider SEBar;
+    public Toggle BGMMute;
+    public Toggle SEMute;
     
 	// Use this for initialization
 	void Start () {
-		
+		if(PlayerPrefs.HasKey("bgm") && PlayerPrefs.HasKey("se")){
+				BGMBar.value = PlayerPrefs.GetFloat("bgm");
+				SEBar.value = PlayerPrefs.GetFloat("se");
+		}
+		else{
+				BGMBar.value = 0;
+				SEBar.value = 0;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+			
 	}
 
     public void MainScene()
     {
+		PlayerPrefs.SetFloat("bgm",BGMBar.value);
+		PlayerPrefs.SetFloat("se",SEBar.value);
         SceneManager.LoadScene("MainScene");
     }
 }
